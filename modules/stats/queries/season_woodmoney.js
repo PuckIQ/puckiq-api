d52b8c644d1595162692efe71f0790320471c358
ssolbak <sean@samdesk.io>
2019-04-02 10:27:28 -0600

    data fix on fo/60

--- modules/stats/queries/season_woodmoney.js
+++ modules/stats/queries/season_woodmoney.js
@@ -131,7 +131,7 @@ module.exports = (mongoose, config) => {
 
                     let rel_comp_stats = {
                         'ozspct': (y.oz / ((y.oz + y.dz) || 1))*100,
-                        'fo60' : ((y.oz+y.nz+y.dz)/(y.evtoi||1))*60,
+                        'fo60' : (y.oz+y.nz+y.dz)/(y.evtoi||1)*3600,
                         'ctoipct': (y.evtoi / (all_toi||1)) * 100,
                         'cf60rc': y.cf60 - off.cf60,
                         'ca60rc': y.ca60 - off.ca60,
