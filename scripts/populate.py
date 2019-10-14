91fdda1682351e322323692ea0392641fb832488
Sean Solbak <sean@samdesk.io>
2019-10-14 09:53:40 -0700

    a few more tweaks to populate.py

--- scripts/populate.py
+++ scripts/populate.py
@@ -3,37 +3,30 @@ import pymongo
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 
-#setup dbs
-wm_client = MongoClient('localhost',
-  username='xxxxxxx',
-  password='xxxxxxx',
-  authSource='nhl'
-)
-pq_client = MongoClient('localhost',
-  29852,
-  username='xxxxxx',
-  password='xxxxxx',
-  authSource='puckiq'
-)
+import config
+_config = config.getFor(os.getenv('PY_ENV') or 'production')
+
+wm_client = MongoClient(_config['dbs']['wm'][0])
+pq_client = MongoClient(_config['dbs']['puckiq'][0])
 
 CURRENT_SEASON= 20192020
-availcol = ['playerhistory','gameboxcars','gamewoodmoney','gamewoodwowy','gamewowy','seasonboxcars','seasonwoodmoney','seasonwoodwowy','seasonwowy','seasonroster']
+collections_to_sync = ['playerhistory','gameboxcars','gamewoodmoney','gamewoodwowy','gamewowy','seasonboxcars','seasonwoodmoney','seasonwoodwowy','seasonwowy','seasonroster']
 wmdb = wm_client.nhl
 pqdb = pq_client.puckiq
 itcount = 0
 
-#build up a dictionary of player/team to games played from the history table
-#todo we may need to do an aggregate here to count the games per player
+#build up a dictionary of player/team to games played from the playerhistory
+#use the wm collection since the puckiq one hasnt been synced yet
 playerhistory = wmdb.get_collection('playerhistory')
 player_dict = dict()
-for player in playerhistory.find({"season": CURRENT_SEASON}):
+for player in playerhistory.find({"season": CURRENT_SEASON, gametype:2 }):
   season_player_key = str(player["playerid"]) + "-" + player["team"]
   player_dict[season_player_key] = player["GP"]
 
-for collection_name in availcol:
+for collection_name in collections_to_sync:
   
   print("\n--------------------------" + collection_name + "--------------------------")
-  rowlection = wmdb.get_collection(collection_name)
+  wm_collection = wmdb.get_collection(collection_name)
   if collection_name == 'nhlroster':
     pqcollection = pqdb.get_collection('seasonroster')
   elif collection_name == 'roster':
@@ -43,14 +36,18 @@ for collection_name in availcol:
     
   if collection_name.find('season') != -1:
     pqcollection.remove({season: CURRENT_SEASON})
+
+  if colName == 'playerhistory':
+      pqcollection.remove({"season": CURRENT_SEASON})
     
-  for row in rowlection.find({"season": CURRENT_SEASON}):
+  for row in wm_collection.find({"season": CURRENT_SEASON}):
     if collection_name.startsWith("season"):
       season_player_key = str(row["playerid"]) + "-" + row["team"]
       if row[season_player_key] in player_dict:
         row["gamesplayed"] = player_dict[season_player_key]
       else
         row["gamesplayed"] = 0
+
     if pqcollection.count(row) < 1:
       pqpostid = pqcollection.insert_one(row).inserted_id
       print("+", end='', flush=True)
